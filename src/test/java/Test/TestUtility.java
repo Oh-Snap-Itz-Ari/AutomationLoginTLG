2edf69a56bddc1725ef41d0c2c41b9028a24c925
Mr. Blue Sky <alexfabianmelo12@hotmail.com>
2024-02-29 08:00:22 -0500

    Fixes Screenshots Allure

--- src/test/java/Test/TestUtility.java
+++ src/test/java/Test/TestUtility.java
@@ -12,7 +12,6 @@ public class TestUtility extends BaseTest{
     public static FileInputStream getScreenshotPathAsFileStream() throws IOException{
         TakesScreenshot scrShot = (TakesScreenshot) driver;
         File srcFile = scrShot.getScreenshotAs(OutputType.FILE);
-
         return new FileInputStream(srcFile);
     }
 
